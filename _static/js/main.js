55a59a61cd7af6ceaa432a5b8328a7bde6e7d371
ffa500 <hubertus.hohl@siemens.com>
2019-12-16 18:13:27 +0100

    feat(xmas): add Xmas surprise

--- _static/js/main.js
+++ _static/js/main.js
@@ -67,5 +67,18 @@ $(function () {
 });
 
 // Swipe functions for Bootstrap Carousel - https://github.com/maaaaark/bcSwipe
-!function(t){t.fn.bcSwipe=function(e){var n={threshold:50};return e&&t.extend(n,e),this.each(function(){function e(t){1==t.touches.length&&(u=t.touches[0].pageX,c=!0,this.addEventListener("touchmove",o,!1))}function o(e){if(c){var o=e.touches[0].pageX,i=u-o;Math.abs(i)>=n.threshold&&(h(),t(this).carousel(i>0?"next":"prev"))}}function h(){this.removeEventListener("touchmove",o),u=null,c=!1}var u,c=!1;"ontouchstart"in document.documentElement&&this.addEventListener("touchstart",e,!1)}),this}}(jQuery);
+!function (t) { t.fn.bcSwipe = function (e) { var n = { threshold: 50 }; return e && t.extend(n, e), this.each(function () { function e(t) { 1 == t.touches.length && (u = t.touches[0].pageX, c = !0, this.addEventListener("touchmove", o, !1)) } function o(e) { if (c) { var o = e.touches[0].pageX, i = u - o; Math.abs(i) >= n.threshold && (h(), t(this).carousel(i > 0 ? "next" : "prev")) } } function h() { this.removeEventListener("touchmove", o), u = null, c = !1 } var u, c = !1; "ontouchstart" in document.documentElement && this.addEventListener("touchstart", e, !1) }), this } }(jQuery);
 $('.carousel').bcSwipe({ threshold: 50 });
+
+// Show Xmas related items at Christmas time
+$(document).ready(function () {
+    var now = new Date();
+    var month = now.getMonth();
+
+    // Xmas time is between Dec 01 and Jan 10
+    if (month === 11 || (month === 0 && now.getDate() <= 10)) {
+        $('.coaty-xmas-item').removeClass('d-none');
+    } else {
+        $('.coaty-xmas-item').addClass('d-none');
+    }
+});
